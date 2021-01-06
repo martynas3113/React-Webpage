6625c89ca60d7c8623502bcc792ab2fccc871498
martynas3113 <martynas3113@users.noreply.github.com>
2021-01-06 13:59:46 +0200

    SidebarAdedd

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import Navbar from './components/navbar';
 import { BrowserRouter as Router } from 'react-router-dom'
+import Sidebar from './components/SideBar';
 
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
   
     </Router>
