0a37cb055b1f7a2767fca7b3a1f542a434833ae5
martynas3113 <martynas3113@users.noreply.github.com>
2021-01-06 20:45:57 +0200

    Structure added

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Video from '../../videos/Video.mp4';
-import { HeroContainer,HeroBack,VideoBg } from './HeroElements';
+import { HeroContainer,HeroBack,VideoBg,HeroContent,HeroH1,HeroP,HeroBtnWrapper,ArrowForward,ArrowRight } from './HeroElements';
 
 const Hero = () => {
+
+    const [hover,setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
     return (
         
         <HeroContainer id="home">
@@ -10,7 +17,13 @@ const Hero = () => {
                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />    
             </HeroBack>
             <HeroContent>
-                
+                <HeroH1>Virtual Data</HeroH1>
+                <HeroP>Sign Up for a new account today!</HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+                        Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
             </HeroContent>
         </HeroContainer>   
     
