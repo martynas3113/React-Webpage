cd59043571a4a7a66390dbad9c6af73108457f44
martynas3113 <martynas3113@users.noreply.github.com>
2021-01-07 22:03:20 +0200

    bottom logo scroll ro top on click

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import {animateScroll as scroll} from 'react-scroll';
 import {FaFacebook, FaInstagram,FaLinkedin,FaGithub} from 'react-icons/fa'
 import{ FooterContainer, FooterWrap, FooterLinksContainer,FooterLinksWrapper,FooterLinkItems, FooterLinkTitle, FooterLink, SocialMedia,SocialMediaWrap,SocialLogo,WebsiteRights,SocialIcons,SocialIconLink } from './footerElements';
 
 const Footer = () => {
+
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    }
     return (
         <FooterContainer>
             <FooterWrap>
@@ -42,7 +47,7 @@ const Footer = () => {
                 </FooterLinksContainer>
                 <SocialMedia>
                     <SocialMediaWrap>
-                        <SocialLogo to='/'>LOGO</SocialLogo>
+                        <SocialLogo to='/' onClick={toggleHome}>LOGO</SocialLogo>
                         <WebsiteRights>Martynas.P © {new Date().getFullYear()}</WebsiteRights>
                         <SocialIcons>
                             <SocialIconLink href='//www.facebook.com/martynas.pazusis/' target='_blank' aria-label="Facebook">
